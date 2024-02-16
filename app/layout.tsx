c7018b96c638db33dfeb46d3810ee700bac2b392
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-16 23:35:35 +0500

    created-prisma-models

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,36 @@
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
-import "./globals.css";
-import {ClerkProvider} from '@clerk/nextjs';
-const inter = Open_Sans({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
+const inter = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Discord Clone",
-  description: "Building Discord Clone",
-};
+  title: 'Discord Clone',
+  description: 'Building Discord Clone'
+}
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <ClerkProvider>
-        {children}
-      </ClerkProvider>
-
+    <html lang='en' suppressHydrationWarning>
+      <body className={
+        cn(inter.className,
+        "bg-white dark:bg-[#313131]"
+        )}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem={false}
+        storageKey="piscord-theme"
+        >
+          <ClerkProvider>{children}</ClerkProvider>
+        </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
